5c5ba0674a396ed17187010f25c5edbf2411b69e
Frans <fjaspers@xs4all.nl>
2022-11-05 22:26:53 +0100

    We can make reservations again

--- ipso-api/blocks/list/mhwp-ipso-list-frontend.js
+++ ipso-api/blocks/list/mhwp-ipso-list-frontend.js
@@ -4,7 +4,8 @@ jQuery.noConflict();
 
 function prepareReservations() {
     // TODO We want this from wp localizeScript, taking MHWP_IPSO__DEV_MODE into account.
-    const url = new URL( "http://marikenhuis.localhost:8080/" );
+    // TODO: Use the production URL.
+    const url = new URL( "http://localhost:8080/" );
     url.pathname = "wp-json/mhwp-ipso/v1/reservation";
 
     /**
@@ -14,23 +15,23 @@ function prepareReservations() {
         return this.optional( element ) || /^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9]){8}$/.test( value );
     }, "Vul een geldig telefoonnummer in." );
 
-    const forms = jQuery('form', '.mhwp-ipso-activities-list');
+    const forms = jQuery('form', '#mhwp-ipso-list-container');
     forms.each( (_, f) => {
-        $( f ).validate({
+        jQuery( f ).validate({
             // We only use one explicit validation rule. others are extracted from the HTML attributes
             rules: {
                 phoneNumber: {
                     phoneNL: true,
-                    normalizer: v => $.trim( v )
+                    normalizer: v => jQuery.trim( v )
                 }
             },
-            submitHandler: function ( form ) {
-                const activityCalendarId = $('input[name="activityCalendarId"]', form).val();
-                const firstName = $('input[name="firstName"]', form).val();
-                const lastNamePrefix = $('input[name="lastNamePrefix"]', form).val();
-                const lastName = $('input[name="lastName"]', form).val();
-                const email = $('input[name="email"]', form).val();
-                let phoneNumber = $('input[name="phoneNumber"]', form).val();
+            submitHandler: function ( form, event ) {
+                const activityCalendarId = jQuery('input[name="activityCalendarId"]', form).val();
+                const firstName = jQuery('input[name="firstName"]', form).val();
+                const lastNamePrefix = jQuery('input[name="lastNamePrefix"]', form).val();
+                const lastName = jQuery('input[name="lastName"]', form).val();
+                const email = jQuery('input[name="email"]', form).val();
+                let phoneNumber = jQuery('input[name="phoneNumber"]', form).val();
                 phoneNumber = phoneNumber === "" ? null : phoneNumber;
                 const data = { activityCalendarId, firstName, lastNamePrefix, lastName, email, phoneNumber };
 
@@ -150,7 +151,7 @@ function addActivities(activities, container) {
             </div>
             
             <div class="collapse reserveer" id="collapseReserveer_${cnt}">
-                <form action="" class="form-horizontal">
+                <form class="form-horizontal">
                     <input type="hidden" name="activityCalendarId" value="${activity.id}">
                         <div class="form-group">
                             <fieldset class="col-md-4">
