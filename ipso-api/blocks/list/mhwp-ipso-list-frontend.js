a14df699d8b6f501a8ece1268fa5f245b0039461
Frans <fjaspers@xs4all.nl>
2022-11-09 19:48:06 +0100

    The frontend html is in a seperate file now

--- ipso-api/blocks/list/mhwp-ipso-list-frontend.js
+++ ipso-api/blocks/list/mhwp-ipso-list-frontend.js
@@ -1,5 +1,7 @@
 import './bootstrap-collapse';
 
+import template from './mhwp-ipso-list-template';
+
 // TODO: Use the production URL.
 const marikenhuisURL ="http://localhost:8080/";
 
@@ -141,8 +143,7 @@ async function addActivities(activities, container) {
 
     for (let key in Object.keys(activities)) {
        let activity = activities[key];
-       let activityDetail = await wait(375).then(() => getActivityDetail(activity.activityID, container));
-       console.log(activityDetail);
+       let activityDetail = await wait(400).then(() => getActivityDetail(activity.activityID, container));
 
        activity.date = activity.onDate.replace(/T\d\d:\d\d:\d\d/, '');
        activity.time = activity.onDate.replace(/\d{4}-\d{2}-\d{2}T/, '');
@@ -155,66 +156,9 @@ async function addActivities(activities, container) {
        light_dark = light_dark === 'light' ? 'dark' : 'light';
        cnt++;
 
-       const html = `
-           <li class="activity list-group-item list-group-item-${light_dark }">
-            <div class="row lead">
-                <div class="col-md-8">
-                    <span>${activity.title}</span>
-                    <span>${activity.date}</span>
-                    <span>${activity.time}</span>
-                    ${activity.img ? activity.img : ''}
-                </div>
-                <div class="col-md-4">
-                    <button class="pull-right btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample_${cnt}" aria-expanded="false" aria-controls="collapseExample">
-                        Lees meer
-                    </button>
-                    <button class="pull-right btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseReserveer_${cnt}" aria-expanded="false" aria-controls="collapseReserveer">
-                        Reserveer
-                    </button>
-                </div>
-            </div>
-            
-            <div class="collapse reserveer" id="collapseReserveer_${cnt}">
-                <form class="form-horizontal">
-                    <input type="hidden" name="activityCalendarId" value="${activity.id}">
-                        <div class="form-group">
-                            <fieldset class="col-md-4">
-                                <label for="mhwp_ipso_voornaam_${cnt}">Voornaam</label>
-                                <span class="required">*</span>
-                                <input type="text" class="form-control" id="mhwp_ipso_voornaam_${cnt}" name="firstName" required placeholder="">
-                            </fieldset>
-                            <fieldset class="col-md-4">
-                                <label for="mhwp_ipso_tussenvoegsel_${cnt}">Tussenvoegsel</label>
-                                <input type="text" class="form-control" id="mhwp_ipso_tussenvoegsel_${cnt}" name="lastNamePrefix" placeholder="">
-                            </fieldset>
-                            <fieldset class="col-md-4">
-                                <label for="mhwp_ipso_achternaam_${cnt}">Achternaam</label>
-                                <span class="required">*</span>
-                                <input type="text" class="form-control" id="mhwp_ipso_achternaam_${cnt}" name="lastName" required placeholder="">
-                            </fieldset>
-                        </div>
-                        <div class="form-group">
-                            <fieldset class="col-md-4">
-                                <label for="mhwp_ipso_telefoon_${cnt}">Telefoonnummer</label>
-                                <input type="tel" class="form-control" id="mhwp_ipso_telefoon_${cnt}" name="phoneNumber" placeholder="">
-                                    <span class="validity"></span>
-                            </fieldset>
-                            <fieldset class="col-md-4">
-                                <label for="mhwp_ipso_email_${cnt}">Emailadres</label>
-                                <span class="required">*</span>
-                                <input type="email" class="form-control" id="mhwp_ipso_email_${cnt}" name="email" required placeholder="">
-                                    <span class="validity"></span>
-                            </fieldset>
-                            <div class="col-md-4">
-                                <button type="submit" class="pull-right right btn btn-default">Reserveer</button>
-                            </div>
-                        </div>
-                </form>
-            </div>
-        </li>`;
-
-        const node = $(html);
-        $(container).append(node);
+       const html = template(activity, cnt, light_dark);
+       const node = $(html);
+       $(container).append(node);
     }
 
     prepareReservations();
@@ -240,7 +184,6 @@ function getActivityDetail(activityId, container) {
         if ( json.mhwp_ipso_status !== 'ok' ) {
             if (json.mhwp_ipso_code === 429) {
                 console.log('Too many requests (429)');
-                return "to many requests;";
             }
             const message = json.mhwp_ipso_msg ? json.mhwp_ipso_msg : '';
             throw new TypeError( message );
