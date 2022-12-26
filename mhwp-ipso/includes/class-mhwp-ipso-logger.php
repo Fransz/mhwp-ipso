e8a74cc29e75f61cd3aec7636a04fa5f8b4b56e4
Frans <fjaspers@xs4all.nl>
2022-12-26 11:45:58 +0100

    Ignoring the logfile

---
+++ mhwp-ipso/includes/class-mhwp-ipso-logger.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * Short description for file
+	 *
+	 * @package
+	 * @author   Frans Jaspers <fjaspers@xs4all.nl>
+	 * @license  https://opensource.org/licenses/gpl-2.0.php GPL2
+	 * @link     https://www.fransjaspers.com
+	 */
+
+	/**
+	 * Class short description
+	 *
+	 * @package
+	 * @author   Frans Jaspers <frans.jaspers@xs4all.nl>
+	 * @license  https://opensource.org/licenses/gpl-2.0.php GPL2
+	 * @link     https://www.fransjaspers.com
+	 */
+	class MHWP_IPSO_Logger {
+
+	}
