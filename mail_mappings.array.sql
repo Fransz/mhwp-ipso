495dc64e579e88461ac0b430d67440d0a0485ce2
Frans <fjaspers@xs4all.nl>
2023-07-01 20:55:32 +0200

    Delete the mailmappings backup

--- mail_mappings.array.sql
+++
@@ -1,9 +0,0 @@
-UPDATE wp_options 
-SET option_value = 'a:11:{i:68;s:47:"fhjkonings@hotmail.com,webmaster@marikenhuis.nl";i:33;s:46:"niekedeling@gmail.com,webmaster@marikenhuis.nl";i:13;s:76:"lichaamsgericht@marikenhuis.nl,info@goedinjelijf.eu,webmaster@marikenhuis.nl";i:29;s:81:"lichaamsgericht@marikenhuis.nl,carrygroot@glazenkamp.net,webmaster@marikenhuis.nl";i:15;s:78:"lichaamsgericht@marikenhuis.nl,simicruzica8@gmail.com,webmaster@marikenhuis.nl";i:8;s:43:"lef@marikenhuis.nl,webmaster@marikenhuis.nl";i:14;s:81:"lichaamsgericht@marikenhuis.nl,anke@laatjelijfspreken.nl,webmaster@marikenhuis.nl";i:11;s:55:"lichaamsgericht@marikenhuis.nl,webmaster@marikenhuis.nl";i:9;s:73:"lichaamsgericht@marikenhuis.nl,info@karenbalk.nl,webmaster@marikenhuis.nl";i:5;s:77:"lichaamsgericht@marikenhuis.nl,aldateeuwen@online.nl,webmaster@marikenhuis.nl";i:4;s:55:"lichaamsgericht@marikenhuis.nl,webmaster@marikenhuis.nl";}'
-WHERE option_name = 'mhwp_ipso_mail_mappings'
-LIMIT 1;
-
---  UPDATE wp_options
---  SET option_value = 'a:0:{}'
---  WHERE option_name = 'mhwp_ipso_mail_mappings'
---  LIMIT 1;
