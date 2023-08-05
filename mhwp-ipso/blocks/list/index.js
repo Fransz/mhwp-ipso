5eb65f31ccfce3d2b7b571528c1942ffd7c5c419
Frans <fjaspers@xs4all.nl>
2023-08-05 14:28:54 +0200

    Dropped the obsolete attribute from the wp-block

--- mhwp-ipso/blocks/list/index.js
+++ mhwp-ipso/blocks/list/index.js
@@ -6,50 +6,19 @@ import './mhwp-ipso-list.scss';
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 import{ InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import {
-    PanelBody,
-    PanelRow,
-    TextControl
-} from '@wordpress/components';
 
 registerBlockType( metadata, {
     icon: icon(),
     edit: ( props ) => {
         const blockProps = useBlockProps();
 
-        return [
-            <InspectorControls>
-                <PanelBody title={ __( 'Basics', 'mhwp-ipso' )}>
-                    <PanelRow>
-                        <p>{ __( 'Configureer de IPSO agenda lijst hier.', 'mhwp-ipso' ) }</p>
-                    </PanelRow>
-
-                    <TextControl
-                        value={ props.attributes.nr_days }
-                        label={ __( 'Aantal dagen vooruit', 'mhwp-ipso' ) }
-                        help={ __( 'Aantal dagen dat in de kalender getoond wordt.', 'mhwp-ipso' ) }
-                        onChange={ (  new_value  ) => {
-                            if ( new_value === '' ) new_value = '10';
-
-                            if (/^\d+$/.test(new_value)) {
-                                props.setAttributes( { nr_days: new_value })
-                            }
-                        }}
-                    />
-
-                </PanelBody>
-            </InspectorControls>,
-
+        return (
             <div { ...blockProps } >
-                <h4>IPSO agenda lijst</h4>
-                <ul className="list-unstyled">
-                    <li>
-                        <span>{ __( 'Aantal dagen vooruit', 'mhwp-ipso' ) }: </span>
-                        <span className="mhwp-ipso-nr-days">{ props.attributes.nr_days }</span>
-                    </li>
-                </ul>
+                <div id="mhwp-ipso-list-backend">
+                    <h4>IPSO agenda lijst</h4>
+                </div>
             </div>
-        ]
+        )
     },
     save: ({attributes}) => {
         const blockProps = useBlockProps.save();
